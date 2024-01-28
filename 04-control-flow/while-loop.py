ef8513e622b26844a5a382786058b604eb7a9c03
Piyush Mishra <piyushrmishra143@gmail.com>
2024-01-28 12:22:44 +0000

    update

---
+++ 04-control-flow/while-loop.py
@@ -0,0 +1,42 @@
+
+
+# i = 1
+
+# while i<=10:
+#     j = 0
+
+#     while j<5:
+#         print(j, "inner loop")
+#         j += 1 
+
+#     print("hello", i)
+#     i = i + 1
+
+#     print("end of outer loop")
+
+
+
+
+
+'''
+* * * * * 
+* * * * * 
+* * * * * 
+* * * * * 
+* * * * * 
+'''
+
+
+
+
+
+# i = 1
+# while i<=5:
+#     j = 1
+#     while j <= 5:
+#         print("*", end = "")
+#         j +=1
+#     print()
+#     i += 1
+
+
